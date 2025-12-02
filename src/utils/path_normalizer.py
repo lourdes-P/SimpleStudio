c5efa6e06a7fc13e2bd169858a1d6d90454093ce
lourdes-P <lourdespanzone@gmail.com>
2025-12-01 22:09:01 -0300

    Modified PathNormalizer resource_path to work with other bundle tools
    resource_path now should return a usable path when executable is built with a tool that sets sys.frozen or that is Nuitka (sets __compile__)
    
    Modified .gitignore to ignore build directories and files

--- src/utils/path_normalizer.py
+++ src/utils/path_normalizer.py
@@ -1,6 +1,7 @@
 
 import os
 import sys
+import tempfile
 
 class PathNormalizer:
     """
@@ -11,7 +12,15 @@ class PathNormalizer:
         """
         Parses a path to obtain an absolute path to resource.
         """
-        base_path = getattr(sys, '_MEIPASS', os.path.abspath('.'))
+        """
+        Works for either Nuitka 
+        (tested, probably works for other programs that build executable with the interpreter located in 
+        the root of the temp directory where the bundled files are extracted) 
+        or PyInstaller"""
+        if hasattr(sys, 'frozen') or '__compiled__' in globals():
+            base_path = getattr(sys, '_MEIPASS', os.path.dirname(sys.executable))
+        else:
+            base_path = os.path.abspath('.')
         
         return os.path.join(base_path, relative_path)
         
